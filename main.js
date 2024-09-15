3bb07a130659cea6aca6bc051f4f34bef1efdac9
Lomooon <Alfarsi.loura@gmail.com>
2024-09-15 14:03:26 +0300

    Project initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let gridDiv = document.getElementsByClassName('gridDiv')[0]
+
+let link = "https://rickandmortyapi.com/api/character";
+
+fetch(link).then(response => response.json()).then(data => {
+    console.log(data) 
+    console.log(data.results);
+    data.results.map(item => {
+        let box = document.createElement('div')
+        box.classList.add('card')
+        let name = document.createElement('p')
+        let img = document.createElement('img')
+
+        name.textContent = item.name
+        img.src = item.image
+        box.appendChild(name)
+        box.appendChild(img)
+        gridDiv.appendChild(box)
+    })
+}
+)
